8933dc7b736bb698549d75c718004c4704c71d8a
Jason Ko <JasonKo@Jasons-MacBook-Pro.local>
2017-09-01 10:15:36 -0400

    Lunar Project Complete

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -5,10 +5,10 @@ import {NavLink} from 'react-router-dom';
 export default class HomePage extends Component {
     render() {
         return (
-            <div className ="clearfix">
+            <div className ="clearfix bigDiv">
                 <div className ="main-body-container">
                     <div className = "main-body-style">
-                        <img src = "http://www.powerpointhintergrund.com/uploads/2017/06/pics-photos-night-sky-hd-wallpaper-150x150-night-sky-hd-wallpaper-6.jpeg" />
+                        <h1 id = "lunar-academy-title">Welcome to Lunar Academy</h1>
                     </div>
                 </div>
             </div>
